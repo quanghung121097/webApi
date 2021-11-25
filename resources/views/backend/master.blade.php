98d650ef4811957d4e60cb092b260e8be006250e
QuangHung <quanghung121097@gmail.com>
2021-11-25 21:54:19 +0700

    no message

--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -196,7 +196,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; DONG HO XUA 2020</span>
+                        <span>Copyright &copy;</span>
                     </div>
                 </div>
             </footer>
